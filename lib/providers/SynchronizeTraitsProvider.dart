4697db3cd13cc2d2288955a144fa06f0fd4385e8
rog33zy <rodgersiradukunda@gmail.com>
2021-11-05 13:10:15 +0200

    post flowering objects method added

--- lib/providers/SynchronizeTraitsProvider.dart
+++ lib/providers/SynchronizeTraitsProvider.dart
@@ -92,16 +92,6 @@ class SynchronizeTraitsProvider with ChangeNotifier {
   Future<void> postPostPlantingObjectsToServer() async {
     for (int i = 0; i < postPlantingObjectsToBeSynced!.length; i++) {
       final unsyncedObject = postPlantingObjectsToBeSynced![i];
-      // final checkObjectExistsUrl = Uri.parse(
-      //   'https://${BaseUrls.sezilUrl}/api/traits/check_post_planting_object/${unsyncedObject.id}/',
-      // );
-      // final checkObjectExistsResponse = await http.get(
-      //   checkObjectExistsUrl,
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'Authorization': 'Token $accessToken',
-      //   },
-      // );
       String checkObjectExistsResponse = unsyncedObject.existsInServer;
       var body = {
         'id': unsyncedObject.id,
@@ -136,6 +126,55 @@ class SynchronizeTraitsProvider with ChangeNotifier {
           'seedlingVigourComments': responseData['seedling_vigour_comments'],
           'plantStand': responseData['plant_stand'],
           'plantStandComments': responseData['plant_stand_comments'],
+          'pestResistance': responseData['pest_resistance'],
+          'pestResistanceComments': responseData['pest_resistance_comments'],
+          'diseasesResistance': responseData['diseases_resistance'],
+          'diseasesResistanceComments':
+              responseData['diseases_resistance_comments'],
+          'isUpToDateInServer': 'Yes',
+          'existsInServer': 'Yes',
+        },
+      );
+      notifyListeners();
+    }
+  }
+
+  Future<void> postFloweringObjectsToServer() async {
+    for (int i = 0; i < floweringObjectsToBeSynced!.length; i++) {
+      final unsyncedObject = floweringObjectsToBeSynced![i];
+      String checkObjectExistsResponse = unsyncedObject.existsInServer;
+      var body = {
+        'id': unsyncedObject.id,
+        'farmer_id': farmerId,
+        'last_updated': unsyncedObject.lastUpdated.toIso8601String(),
+        'plot_id': unsyncedObject.plotId,
+        'growing_cycle_appreciation': unsyncedObject.growingCycleAppreciation,
+        'growing_cycle_appreciation_comments':
+            unsyncedObject.growingCycleAppreciationComments,
+        'pest_resistance': unsyncedObject.pestResistance,
+        'pest_resistance_comments': unsyncedObject.pestResistanceComments,
+        'diseases_resistance': unsyncedObject.diseasesResistance,
+        'diseases_resistance_comments':
+            unsyncedObject.diseasesResistanceComments,
+      };
+
+      final Map<String, dynamic> responseData =
+          await postUnsyncedObjectsToServer(
+        body,
+        'traits/flowering_objects',
+        checkObjectExistsResponse,
+        unsyncedObject,
+      );
+      await DBHelper.insert(
+        'postPlanting',
+        {
+          'id': responseData['id'],
+          'lastUpdated': responseData['last_updated'],
+          'plotId': responseData['plot_id'],
+          'growingCycleAppreciation':
+              responseData['growing_cycle_appreciation'],
+          'growingCycleAppreciationComments':
+              responseData['growing_cycle_appreciation_comments'],
           'pestResistance': responseData['pest_resistance'],
           'pestResistanceComments': responseData['pest_resistance_comments'],
           'diseasesResistance': responseData['diseases_resistance'],
